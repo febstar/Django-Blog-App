a9b8e15e0c1d9f62440daad72a86ce4f90b74e44
febstar <febechukwuonyeyili@gmail.com>
2024-11-26 20:25:27 +0100

    Version 1.1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './BlogApp/Blog/templates/**/*.html',
+    './BlogApp/static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
